381bc40602df67c314a2be712d634d0db568cfa5
Nadiia1701 <146233511+Nadiia1701@users.noreply.github.com>
2024-05-03 08:51:49 -0400

    Update ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { selectContacts, selectNameFilter } from "../../redux/selectors";
 
 export default function ContactList() {
-  const contacts = useSelector(selectContacts);
+  const contacts = useSelector((state) => selectContacts(state).items);
   const filter = useSelector(selectNameFilter);
 
   const visibleContacts = contacts.filter((contact) =>
